ad262638c1065842878017504970d964980cdedc
Andre Carrera <acarrera94@gmail.com>
2017-08-30 17:14:45 -0600

    Created project, added basic buttons and text field

---
+++ RedBlueBPM/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  RedBlueBPM
+//
+//  Created by Andre Carrera on 8/30/17.
+//  Copyright © 2017 Carrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var tapableButton: UIButton!
+    @IBOutlet weak var bpmText: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
